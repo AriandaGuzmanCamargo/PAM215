a249cb793438fcc9e1725d0c4f377ae2826152e9
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-09-09 10:18:09 -0600

    Variables
    Se Crea una variable la cual puede ser  remplazada por otro valor

--- JSparaPrincipiantes/app.js
+++ JSparaPrincipiantes/app.js
@@ -1,26 +1,5 @@
-//tipos de datos
-"Hola Mundo" //string
-'Hola Mundo' //string
-123 //number
-123.45 //number decimal
-true //boolean
-false //boolean
+var nameusuer = "john";
+let lastame = 'cater';
 
-//Array
-['jose','ryan','martha'] //array de strings o lista de usuario
-[1,2,3,4,5] //array de numeros
-[true,false,true] //array de booleanos
-[1,'jose',true] //array mixto
-
-//objeto
-'ryan'
-70.4
-14
-true
-{
-    "username": 'ryan',
-    "score": 70.4,
-    "hours": 14,
-    "proffessional": true
-    
-}
+nameuser = 'pepe';
+console.log(nameusuer);
