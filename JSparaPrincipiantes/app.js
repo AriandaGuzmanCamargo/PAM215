786886f69b65f6f45b49b8929057adb18e3ac2b5
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-09-09 11:32:29 -0600

    Funciones

--- JSparaPrincipiantes/app.js
+++ JSparaPrincipiantes/app.js
@@ -1,6 +1,18 @@
-//bucle
+//bucles
 let count = 50;
-while(count>0){
+while(count<0){
     console.log(count);
     count--;
 }
+
+let names = ['Ana', 'Pepe', 'Luis', 'Maria'];
+
+for(let i=0; i<3; i++){
+    console.log(names[i]);
+}
+
+function greetting(){
+    console.log('Hola');
+}
+greetting();
+greetting();s
