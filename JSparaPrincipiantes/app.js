c969017fec049750bb1cbf2d50148c04bb61e48c
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-09-09 11:06:54 -0600

    switch
    son casos que pueden suceder depende lo que ingrese el usuario

--- JSparaPrincipiantes/app.js
+++ JSparaPrincipiantes/app.js
@@ -1,19 +1,14 @@
-let passwordBD = 'pepe123';
-let input = 'pepe123';
 
-let result = input == passwordBD;
+let typeCard = 'Debid Card';
 
-if (result == true) {
-    console.log('Login correcto');
-}else{
-    console.log('Password incorrecto');
-}
-
-let score = 70;
-if (score >= 30) {
-    console.log('You need to practice more ');
-}else if(score >15){
-    console.log('Estas mejorando');
-}else {
-    console.log('Yoy need to follow this tutorial');
+switch (typeCard) {
+    case 'Debid Card':
+        console.log('esta es una tarjeta de debito');
+        break;
+    case 'Credit Card':
+        console.log('esta es una tarjeta de credito');
+        break;
+    default:
+        console.log('No card');
+        break;
 }
