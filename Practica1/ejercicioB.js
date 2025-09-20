14253359dbb8c7e63ae5e7a903000ff9f536c162
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-09-19 20:25:57 -0600

    Ejercicio B Cuadrado de un numero
    Se agrego una funcion de flecha llamada cuadrado 2 para poder seducir el numero de lineas, y 3 console por cada numero de prueba.

---
+++ Practica1/ejercicioB.js
@@ -0,0 +1,9 @@
+function cuadrado(numero){
+    return numero * numero;
+}
+console.log(cuadrado(2));
+
+const cuadrado2 = numero => numero * numero;
+console.log(cuadrado2(5)); 
+console.log(cuadrado2(10)); 
+console.log(cuadrado2(3)); 
