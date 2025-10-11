47212b5586e3da7a3b270fb91954fe294331d627
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-10-10 20:18:50 -0600

    Practica 5
    En esta practica se identificaron las zonas  que componen nuestro proyecto, al igual que creamos 3 botones con funcionalidades diferentes, los 3 son contadores pero uno aumentara, otro decrecera y el otro reiniciara

--- intro/App.js
+++ intro/App.js
@@ -1,19 +1,31 @@
+//1. imports:Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import React, { useState } from 'react';
 
+//2. Main: Zona de componentes
 export default function App() {
+
+  const[contador,setContador] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola mundo </Text>
+
+      <Text>Contador:{contador}</Text>
+
+      <Button title='Incrementar'onPress={()=>setContador(contador+1)}> </Button>
+      <Button title='Quitar'onPress={()=>setContador(contador-1)}> </Button>
+      <Button title='Reiniciar'onPress={()=>setContador(0)}> </Button>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//3. Styles: Zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    color:'black',
+    flex: 2,
+    backgroundColor: '#ffffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
