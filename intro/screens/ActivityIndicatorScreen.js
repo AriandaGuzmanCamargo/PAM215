1ea3fcfc9a0a1379ac1414d13531fde373f7c367
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-10-28 20:28:19 -0600

    ActivityIndicator: Completada

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,12 +1,58 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, StyleSheet, View, Button, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
 
-export default function ActivityIndicator() {
-    return (
-      <View>
-        <Text>Proximamente por Leonel</Text>
+export default function ActivityIndicatorScreen() {
+  const [loading, setLoading] = useState(false);
+  const startLoading = () => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 3000);
+  };
+  if(loading){
+    return(
+    <View style={styles.container}>
+      <View >
+        <ActivityIndicator 
+          size="large" 
+          color="#c33decff" 
+          animating={true}
+          hidesWhenStopped={true}
+        />
+        <Text style={styles.cargando}>Cargando.......</Text>
       </View>
-    )
+    </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>ActivityIndicator</Text>
+      <Button color='#6e058cff' title='Iniciar carga' onPress={startLoading}/>
+    </View>
+  );
+
+  
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#a8e0f8ff',
+
+  },
+  titulo:{
+    fontSize:20,
+    fontWeight:'bold',
+    alignItems:'center',
+    justifyContent:'center',
+    marginBottom:20,
+  },
+  cargando:{
+    fontSize:16,
+    fontWeight:'normal',
+    alignItems:'center',
+    justifyContent:'center',
+    marginTop:10,
+    color:'#cb0404ff',
+  },
+})
