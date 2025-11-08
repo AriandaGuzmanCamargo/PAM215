bfa7a6fbe2af7a6506909b7680840789452bbe30
Dulce Ariadna Guzman Camargo <124051075@upq.edu.mx>
2025-11-07 20:31:26 -0600

    Examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View, Button, ImageBackground,Animated,Easing,ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground,Animated,Easing,ScrollView,Switch } from 'react-native';
 import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
   const [cargando,setCargando] = useState(true);
   const desvanecido = new Animated.Value(1);
-
+  const [esEncendido, cambiarEncendido] = useState(false);
+  const [esEncendidoT1, cambiarEncendidoT1] = useState(false);
+  const [esEncendidoT2, cambiarEncendidoT2] = useState(false);
+  const [esEncendidoT3, cambiarEncendidoT3] = useState(false);
+  const [esEncendidoT4, cambiarEncendidoT4] = useState(false);
+  const [esEncendidoT5, cambiarEncendidoT5] = useState(false);
+  const [esEncendidoT6, cambiarEncendidoT6] = useState(false);
   
   useEffect(()=>{
     const timer = setTimeout(() => {
@@ -33,40 +39,105 @@ export default function App() {
   }
 
   return (
+    <ScrollView>
     <View style={styles.container}>
+      
       <ImageBackground
         source={require('./assets/fondo2.jpeg')}
         resizeMode='cover'
         style={styles.background}
       >
-        <Text>"Mis deberes"</Text>
+        <Text style={styles.tituloP}>"Mis deberes"</Text>
         <Text>Viernes 07 de Noviembre del 2025</Text>
         <View>
-          <ScrollView 
-          >
+          
             <View style={styles.contenedoresP}>
               <Text style={styles.titulos}>Personal</Text>
-              <Text style={styles.contenedoresP}>Sepillarse los dientes</Text>
-              <Text>Bañarse</Text>
+              <View style={styles.tareasC}>
+                <Text >Sepillarse los dientes</Text>
+                <Text>Descripccion: Cada dia sepillar los dientes en las mananas</Text>
+                <Text>Prioridad: ALta</Text>
+                <Switch
+                  value={esEncendidoT1}
+                  onValueChange = {()=> cambiarEncendidoT1(!esEncendidoT1)}
+                  trackColor={{true: 'yellow', false: 'gray'}}
+                > </Switch>
+              </View>
+              <View style={styles.tareasC}>
+                <Text >Bañarse</Text>
+                <Text>Descripccion: 2 veces al dia banarse</Text>
+                <Text>Prioridad:ALta</Text>
+                <Switch
+                  value={esEncendidoT2}
+                  onValueChange = {()=> cambiarEncendidoT2(!esEncendidoT2)}
+                  trackColor={{true: 'yellow', false: 'gray'}}
+                ></Switch>
+              </View>
+              
             </View>
             <View style={styles.contenedoresE}>
               <Text style={styles.titulos}>Estudios</Text>
-              <Text>Hacer examen de react native</Text>
-              <Text>Estudiar para el examen de matematicas</Text>
+                <View style={styles.tareasC}>
+                  <Text>Hacer examen de react native</Text>
+                <Text>Descripccion: Estuaidar y practicar para el examen</Text>
+                <Text>Prioridad: Alta</Text>
+                <Switch
+                  value={esEncendidoT3}
+                  onValueChange = {()=> cambiarEncendidoT3(!esEncendidoT3)}
+                  trackColor={{true: 'yellow', false: 'gray'}}
+                ></Switch>
+              </View>
+              <View style={styles.tareasC}>
+                <Text>Estudiar para el examen de matematicas</Text>
+              <Text>Descripccion: Practicar ejercicios</Text>
+              <Text>Prioridad: ALta</Text>
+              <Switch
+                value={esEncendidoT4}
+                onValueChange = {()=> cambiarEncendidoT4(!esEncendidoT4)}
+                trackColor={{true: 'yellow', false: 'gray'}}
+              ></Switch>
+              </View>
+              
             </View>
             <View style={styles.contenedoresH}>
               <Text style={styles.titulos}>Hogar</Text>
-              <Text>Lavar los trastes</Text>
-              <Text>Barrer la casa</Text>
-            </View>
+              <View style={styles.tareasC}>
+                <Text>Lavar los trastes</Text>
+              <Text>Descripccion: Limpiar y lavar bien los trastes sucios</Text>
+              <Text>Prioridad: Media</Text>
+              <Switch
+                value={esEncendidoT5}
+                onValueChange = {()=> cambiarEncendidoT5(!esEncendidoT5)}
+                trackColor={{true: 'yellow', false: 'gray'}}
+              ></Switch>
+              </View>
               
-          </ScrollView>
+              <View style={styles.tareasC}>
+                <Text>Barrer la casa</Text>
+              <Text>Descripccion: Barrer el patio y recojer basura</Text>
+              <Text>Prioridad: Baja</Text>
+              <Switch
+                value={esEncendidoT6}
+                onValueChange = {()=> cambiarEncendidoT6(!esEncendidoT6)}
+                trackColor={{true: 'yellow', false: 'gray'}}
+              ></Switch>
+              </View>
+              
+            </View>
+            <Button
+              title='Reiniciar'
+              onPress={()=> esEncendidoT6 && cambiarEncendidoT6 }
+            />
+              
+        
         </View>
         
 
 
       </ImageBackground>
+      
     </View>
+    </ScrollView>
   );
 }
 
@@ -98,22 +169,29 @@ const styles = StyleSheet.create({
     height:'100%',
   },
   contenedoresP:{
-    flex:1,
     padding:20,
+    height:'50%',
     marginTop:50,
     backgroundColor:'rgba(247, 194, 194, 0.8)',
     borderRadius:10,
 
   },
-  contenedoresE:{
-    flex:1,
+  tareasC:{
     padding:20,
+    height:'30%',
+    marginTop:50,
+    backgroundColor:'rgba(147, 147, 145, 0.8)',
+    borderRadius:10,
+  },
+  contenedoresE:{
+    padding:20,
+    height:'50%',
     marginTop:50,
     backgroundColor:'rgba(219, 247, 194, 0.8)',
     borderRadius:10,
   },
   contenedoresH:{
-    flex:1,
+    height:'50%',
     padding:20,
     marginTop:50,
     backgroundColor:'rgba(194, 227, 247, 0.8)',
@@ -122,5 +200,16 @@ const styles = StyleSheet.create({
   titulos:{
     fontSize:30,
 
+  },
+  tareas:{
+    width:'100%',
+    height:'100%',
+    backgroundColor:'rgba(194, 227, 247, 0.8)',
+    flex:1,
+    marginBottom:20,
+  },
+  tituloP:{
+    fontSize:40,
+    color:'red',
   },
 });
